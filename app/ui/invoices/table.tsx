f5b82f69101964da3c414f374ef89b1c27ad64b4
semosem <sem.gebresilassie@gmail.com>
2024-05-18 20:08:10 +0300

    feat: finish invoice page

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,6 +11,8 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
+  console.log('searching invoices', 'table', `query ${query}`, currentPage);
+
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
