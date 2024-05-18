8311da15144807926508c47a61ca7418193f943a
semosem <sem.gebresilassie@gmail.com>
2024-05-18 20:05:35 +0300

    update: packages

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -4,7 +4,7 @@ const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
-    email: 'user@nextmail.com',
+    email: 'semosem@github.com',
     password: '123456',
   },
 ];
@@ -12,63 +12,57 @@ const users = [
 const customers = [
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
-    email: 'delba@oliveira.com',
+    name: 'Getenesh Kidane',
+    email: 'sem@gebresilassie.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
-    email: 'lee@robinson.com',
+    name: 'Mehalet Yared',
+    email: 'mehalet@yared.com',
     image_url: '/customers/lee-robinson.png',
   },
   {
     id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
-    name: 'Hector Simpson',
+    name: 'Mekbib Kidane',
     email: 'hector@simpson.com',
     image_url: '/customers/hector-simpson.png',
   },
   {
     id: '50ca3e18-62cd-11ee-8c99-0242ac120002',
-    name: 'Steven Tey',
+    name: 'Lemlem Kidane',
     email: 'steven@tey.com',
     image_url: '/customers/steven-tey.png',
   },
   {
     id: '3958dc9e-787f-4377-85e9-fec4b6a6442a',
-    name: 'Steph Dietz',
+    name: 'Kidane Gebresilassie',
     email: 'steph@dietz.com',
     image_url: '/customers/steph-dietz.png',
   },
   {
     id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
-    name: 'Michael Novotny',
+    name: 'Tirunesh Sarhe',
     email: 'michael@novotny.com',
     image_url: '/customers/michael-novotny.png',
   },
   {
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
+    name: 'Giz Kidane',
     email: 'evil@rabbit.com',
     image_url: '/customers/evil-rabbit.png',
   },
   {
     id: '126eed9c-c90c-4ef6-a4a8-fcf7408d3c66',
-    name: 'Emil Kowalski',
+    name: 'Desu Kidane',
     email: 'emil@kowalski.com',
     image_url: '/customers/emil-kowalski.png',
   },
   {
-    id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
-    name: 'Amy Burns',
-    email: 'amy@burns.com',
-    image_url: '/customers/amy-burns.png',
-  },
-  {
-    id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
-    name: 'Balazs Orban',
-    email: 'balazs@orban.com',
-    image_url: '/customers/balazs-orban.png',
+    id: '126eed9c-c90c-4ef6-a4a8-fcf7408d3c36',
+    name: 'Adanu Kidane',
+    email: 'emil@kowalski.com',
+    image_url: '/customers/emil-kowalski.png',
   },
 ];
 
